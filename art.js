9a546c79382bb055f4f1ad5752a3af94a03bdece
Benji Sloan <sloanrobe@hotmail.com>
2025-01-02 13:39:41 +0100

    Added aquare and triangle functions

--- art.js
+++ art.js
@@ -1,1 +1,29 @@
 // TODO Add functionality for drawing shapes
+function drawLineOfStars(length) {
+    
+    let rowOfStars = ''
+    
+    for (let starCount = 0; starCount < length; starCount++) {
+        rowOfStars = rowOfStars + '*'
+    }
+    
+    console.log(rowOfStars)
+
+    if (answer === triangle) {
+        function drawTriangle(height) {
+            for (let rowCount = 1; rowCount <= height; rowCount++) {
+                drawLineOfStars(rowCount)
+            }
+        }
+    } else if (answer === square) {
+        function drawSquare(size) {
+            for (let row = 0; row < size; row++) {
+                let rowOfStars = '';
+                for (let col = 0; col < size; col++) {
+                    rowOfStars += '*';
+                }
+                console.log(rowOfStars);
+            }
+        }
+    }
+}
