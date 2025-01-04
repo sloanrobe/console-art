628798a61acd0da191ee40d465319f2843ee25f2
Benji Sloan <sloanrobe@hotmail.com>
2025-01-04 14:30:22 +0100

    Moved if statements inside the main function

--- main.js
+++ main.js
@@ -4,20 +4,18 @@ const { askForShape, askForDimension } = require('./input')
 async function main() {
     const shape = await askForShape()
     const dimension = await askForDimension() 
-}
 
-// TODO Use input to draw shapes
-
-if (shape === 'triangle') {
-    drawTriangle(dimension)
-} else if (shape === 'square') {
-    drawSquare(dimension)
-} else if (shape === 'pyramid') {
-    drawPyramid(dimension)
-} else if (shape === 'uPyramid') {
-    drawuPyramid(dimension)
-} else {
-    console.log('Please choose one of the following shapes: triangle, square, pyramid, or uPyramid.')
+    if (input === 'triangle') {
+        drawTriangle(dimension)
+    } else if (input === 'square') {
+        drawSquare(dimension)
+    } else if (input === 'pyramid') {
+        drawPyramid(dimension)
+    } else if (input === 'uPyramid') {
+        drawuPyramid(dimension)
+    } else {
+        console.log('Please choose one of the following shapes: triangle, square, pyramid, or uPyramid.')
+    }
 }
 
 main()
